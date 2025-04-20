26a0292e2c10c156e5b91ed5a80fc880b0e183cf
Anshuman Singh <E21CSEU0743@bennett.edu.in>
2025-04-21 00:31:55 +0530

    Updated Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="space-x-4">
         <Link to="/" className="text-gray-700 hover:text-indigo-500">Home</Link>
         <Link to="/quiz" className="text-gray-700 hover:text-indigo-500">Quiz</Link>
-        <Link to="/debate" className="text-gray-700 hover:text-indigo-500">Debate Room</Link>
+        <Link to="/joindebate" className="text-gray-700 hover:text-indigo-500">Debate Room</Link>
       </div>
     </nav>
   );
